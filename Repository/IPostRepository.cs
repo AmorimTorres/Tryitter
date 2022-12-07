05b3bc8275d034e94fb1edfc10751539376fc523
André Torres <andre_torres91@hotmail.com>
2022-12-07 15:50:24 -0300

    feat: post repository

---
+++ Repository/IPostRepository.cs
@@ -0,0 +1,8 @@
+﻿using Rede_Social_Da_Galera___Tryitter.Models;
+
+namespace Rede_Social_Da_Galera___Tryitter.Repository
+{
+    public interface IPostRepository : IRepository<Post>
+    {
+    }
+}
---
+++ Repository/PostRepository.cs
@@ -0,0 +1,12 @@
+﻿using Rede_Social_Da_Galera___Tryitter.Context;
+using Rede_Social_Da_Galera___Tryitter.Models;
+
+namespace Rede_Social_Da_Galera___Tryitter.Repository
+{
+    public class PostRepository : Repository<Post>, IPostRepository
+    {
+        public PostRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
