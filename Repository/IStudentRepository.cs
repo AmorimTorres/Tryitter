0b119ae82ca53d41994a456e3302cfdcfa67b6bb
André Torres <andre_torres91@hotmail.com>
2022-12-07 11:49:21 -0300

    feat: student repository

---
+++ Repository/IStudentRepository.cs
@@ -0,0 +1,9 @@
+﻿using Rede_Social_Da_Galera___Tryitter.Models;
+
+namespace Rede_Social_Da_Galera___Tryitter.Repository
+{
+    public interface IStudentRepository : IRepository<Student>
+    {
+        IEnumerable<Student> GetStudentOrderedByModule();
+    }
+}
---
+++ Repository/StudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rede_Social_Da_Galera___Tryitter.Context;
+using Rede_Social_Da_Galera___Tryitter.Models;
+
+namespace Rede_Social_Da_Galera___Tryitter.Repository
+{
+    public class StudentRepository : Repository<Student>, IStudentRepository
+    {
+        public StudentRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Student> GetStudentOrderedByModule()
+        {
+            return GetAll().OrderBy(m => m.CourseModule).ToList();
+        }
+    }
+}
