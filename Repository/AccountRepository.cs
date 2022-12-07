1bbcd70fb831694fabfca8fb890904d733953d23
André Torres <andre_torres91@hotmail.com>
2022-12-07 15:49:48 -0300

    feat: Account Repository

---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rede_Social_Da_Galera___Tryitter.Context;
+using Rede_Social_Da_Galera___Tryitter.Models;
+
+namespace Rede_Social_Da_Galera___Tryitter.Repository
+{
+    public class AccountRepository : Repository<Account>, IAccountRepository
+    {
+        public AccountRepository(AppDbContext context) : base(context)
+        {
+        }
+        public IEnumerable<Account> GetAccountPosts()
+        {
+            return GetAll().Include(p => p.Posts);
+        }
+    }
+}
---
+++ Repository/IAccountRepository.cs
@@ -0,0 +1,9 @@
+﻿using Rede_Social_Da_Galera___Tryitter.Models;
+
+namespace Rede_Social_Da_Galera___Tryitter.Repository
+{
+    public interface IAccountRepository : IRepository<Account>
+    {
+        IEnumerable<Account> GetAccountPosts();
+    }
+}
