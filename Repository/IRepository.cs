2b20d1dd2552db8dbc6d0de3a58cc482187e0e40
André Torres <andre_torres91@hotmail.com>
2022-12-07 15:55:54 -0300

    fix: not async repository (yet)

--- Repository/IRepository.cs
+++ Repository/IRepository.cs
@@ -5,7 +5,7 @@ namespace Rede_Social_Da_Galera___Tryitter.Repository
     public interface IRepository<T>
     {
         IQueryable<T> Get();
-        Task<T> GetById(Expression<Func<T, bool>> predicate);
+        T GetById(Expression<Func<T, bool>> predicate);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
